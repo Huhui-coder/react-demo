7e797e91ab0f9509393da94c52d865b933aa8b40
huhui <569263082@qq.com>
2019-10-30 16:11:08 +0800

    未使用react-redux实现的组件之间通信

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class CommentInput extends Component {
+    static propTypes = {
+        onSubmit: PropTypes.func
+    }
     constructor() {
         super()
         this.state = {
             username: '',
             context: ''
         }
+    }
+
+    componentWillMount() {
+        this._loadUsername()
+    }
+    componentDidMount = () => {
+        this.textarea.focus()
+    };
+    _loadUsername() {
+        const username = localStorage.getItem('username')
+        if (username) {
+            this.setState({ username })
+        }
+    }
+    _saveUsername(username) {
+        localStorage.setItem('username', username)
     }
     handleUsernameChange(event) {
         this.setState({
@@ -18,27 +38,34 @@ class CommentInput extends Component {
             context: event.target.value
         })
     }
-    handleSubmit(){  //注意此处的逻辑，我们想要的是 子组件传参数给父组件，父组件再通过props传参给另一个子组件。
+    handleSubmit() {  //注意此处的逻辑，我们想要的是 子组件传参数给父组件，父组件再通过props传参给另一个子组件。
         //子组件传参，用的是 类似vue中的emit-on的形式进行传参的。子组件调用父组件传递过来的函数，将参数用形参的方式传递过去。
-        if(this.props.onSubmit){
-            const {username,context} = this.state;
-            this.props.onSubmit({username,context})
-            this.setState({context:''})
+        if (this.props.onSubmit) {
+            this.props.onSubmit({
+                username: this.state.username,
+                context: this.state.context,
+                createdTime: +new Date()
+            })
+            this.setState({ context: '' })
         }
     }
+    handleUsernameBlur(event) {
+        this._saveUsername(event.target.value)
+    }
+
     render() {
         return (
             <div className='comment-input'>
                 <div className='comment-field'>
                     <span className='comment-field-name'>用户名：</span>
                     <div className='comment-field-input'>
-                        <input value={this.state.username} onChange={this.handleUsernameChange.bind(this)} />
+                        <input value={this.state.username} onChange={this.handleUsernameChange.bind(this)} onBlur={this.handleUsernameBlur.bind(this)} />
                     </div>
                 </div>
                 <div className='comment-field'>
                     <span className='comment-field-name'>评论内容：</span>
                     <div className='comment-field-input'>
-                        <textarea value={this.state.context} onChange={this.handleContextChange.bind(this)} />
+                        <textarea ref={(textarea) => this.textarea = textarea} value={this.state.context} onChange={this.handleContextChange.bind(this)} />
                     </div>
                 </div>
                 <div className='comment-field-button'>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import CommentApp from './CommentApp'
 
 import * as serviceWorker from './serviceWorker';
